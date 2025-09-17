0376fde185708d2a4eb4114c14b0293dd9231c45
Leandro Moren <leandro.moren@outlook.com>
2025-09-17 16:13:30 -0300

    Cambio nombre del logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,9 +4,9 @@ const Logo: React.FC = () => {
   return (
     <div className="flex items-center">
       <span className="text-3xl font-extrabold tracking-tighter">
-        <span className="bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">RE</span>
+        <span className="bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">REFI</span>
         <span className="relative">
-          <span className="bg-gradient-to-r from-blue-400 to-cyan-500 bg-clip-text text-transparent">FI</span>
+          <span className="bg-gradient-to-r from-blue-400 to-cyan-500 bg-clip-text text-transparent">SIM</span>
           <span className="absolute -bottom-1 left-0 w-full h-[2px] bg-gradient-to-r from-blue-600 to-cyan-500"></span>
         </span>
       </span>
